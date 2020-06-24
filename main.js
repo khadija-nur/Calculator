a79c55dcf0de36915f309e2da662eee66ff43a6f
Khadija <khadija.nur99@gmail.com>
2020-06-24 17:36:00 +0100

    add division functionality

--- main.js
+++ main.js
@@ -12,3 +12,15 @@ const displayResults = document.querySelector(".display.results");
 keys.forEach(key => key.addEventListener("click", e => {
     displayExpression.innerHTML += (e.target.textContent);
 }));
+const parseExpressionWithDivision = (expression) =>{
+    const DivisionSeparatedNums = expression.split("÷");
+    const numbers =  DivisionSeparatedNums.map(nums => +nums);
+    const intialValue = numbers[0];
+    // const newNums = numbers.slice(1)
+    // console.log(numbers.slice(1))
+
+    result = numbers.slice(1).reduce((acc, nums) => acc / nums, intialValue )
+    console.log("result with div "+ result) ;
+        return result;
+
+};
