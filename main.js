b3cdc0cd83c08df189cff7b5f3eb94edb3a28783
Khadija <khadija.nur99@gmail.com>
2020-06-29 12:48:33 +0100

    clean code

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const keys = document.querySelectorAll(".key");
 const numbers = document.querySelectorAll(".number");
 const symbols = document.querySelectorAll(".symbol");
 const equalSign = document.querySelector(".equals");
-// const display = document.querySelector("#display");
+
 const displayExpression = document.querySelector(".display.expression");
 const displayResults = document.querySelector(".display.results");
 
@@ -13,22 +13,18 @@ keys.forEach(key => key.addEventListener("click", e => {
     displayExpression.innerHTML += (e.target.textContent);
 }));
 
-
 equalSign.addEventListener("click", e => {
     const expression =  displayExpression.innerHTML;
     return evaluate(expression)
+});
 
-})
 const parseExpressionWithDivision = (expression) =>{
     const DivisionSeparatedNums = expression.split("÷");
     const numbers =  DivisionSeparatedNums.map(nums => +nums);
     const intialValue = numbers[0];
-    // const newNums = numbers.slice(1)
-    // console.log(numbers.slice(1))
 
     result = numbers.slice(1).reduce((acc, nums) => acc / nums, intialValue )
-    console.log("result with div "+ result) ;
-        return result;
+    return result;
 
 };
 
@@ -36,8 +32,8 @@ const parseExpressionWithMultiplication = (expression) =>{
     const MultiplicationSeparatedNums = expression.split("*")
     const numbers = MultiplicationSeparatedNums.map(nums => parseExpressionWithDivision(nums));
     const initialValue = 1.0;
-	const result = numbers.reduce((acc, nums) => acc * nums, initialValue);
-    console.log("result with mult "+ result) ;
+
+    const result = numbers.reduce((acc, nums) => acc * nums, initialValue);
     return result;
 
 };
@@ -46,9 +42,8 @@ const parseExpressionWithPlus = (expression) =>{
     const plusSeparatedNums = expression.split("+")
     const numbers = plusSeparatedNums.map(nums => parseExpressionWithMultiplication(nums));
     const initialValue = 0.0;
+    
 	const result = numbers.reduce((acc, nums) => acc + nums, initialValue);
-
-    console.log("result with plus "+ result) ;
     return result;
 
 }
@@ -58,15 +53,11 @@ const parseExpressionWithminus= (expression) =>{
     const numbers = minusSeparatedNums.map(nums => parseExpressionWithPlus(nums));
     const initialValue = numbers[0];
 	const result = numbers.slice(1).reduce((acc, nums) => acc - nums, initialValue);
-    console.log("result with minus "+ result) ;
     displayResults.innerHTML = result;
+
     return result;
-
-
-
 }
 
-const evaluate = (expression) =>{
-    parseExpressionWithminus(expression);
-}
+const evaluate = (expression) =>parseExpressionWithminus(expression);
+
 
