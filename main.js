4401032a750ebc4be3f7e2076eb73b8aa8d195fe
Khadija <khadija.nur99@gmail.com>
2020-07-05 13:42:16 +0100

    clean code

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // define variable
 const keys = document.querySelectorAll(".key");
-const soundEffect =new Audio('button.mp3')
+const soundEffect = new Audio('button.mp3')
 
 const numberKeys = Array.from(document.querySelectorAll(".number"));
 const operators = Array.from(document.querySelectorAll(".operator"));
@@ -18,121 +18,74 @@ const displayResults = document.querySelector(".display.results");
 let canUseDot;
 let canInputZero = true;
 
-
-
 keys.forEach(key => key.addEventListener("click", e => {
-
-
+    const lastCharacterOnDisplay = displayExpression.innerHTML[displayExpression.innerHTML.length - 1];
+    const secLastCharacterOnDisplay = displayExpression.innerHTML[displayExpression.innerHTML.length - 2];
     // plays clicking key sounds
     soundEffect.play();
-
     // check for unnecessary zeros 
-    if (e.target == zero ){
-        console.log("ehvsd")
+    if (e.target == zero) {
         checkForZeros(e.target, displayExpression.innerHTML)
-        // console.log("e.target is zero")
-        // if (displayExpression.innerHTML == ""){
-        //     console.log("empty display")
-
-        //     displayExpression.innerHTML += e.target.innerHTML
-        //     canInputZero = false;
-        //     console.log(canInputZero)
-        // }
-        // else if (
-        //     operatorStrings.includes(displayExpression.innerHTML[displayExpression.innerHTML.length-2])
-        // ){
-        //     canInputZero == false
-        // }
-        // else if (canInputZero == false){
-        //     console.log(canInputZero)
-
-        //     console.log("zeros should not be here")
-        //     checkForZeros(e.target, displayExpression.innerHTML)
-        //     canInputZero = true;
-        // }
-        // else{
-        //     console.log(canInputZero)
-
-        //     if(operatorStrings.includes(displayExpression.innerHTML[displayExpression.innerHTML.length - 2])){
-        //         displayExpression.innerHTML += e.target.innerHTML
-        //         canInputZero = false;
-        //     }
-        //     else
-        //     displayExpression.innerHTML += e.target.innerHTML
-        // }
-      
     }
-    
-    else if (!operators.includes(e.target)){
 
-        if (e.target !== dot){
-            displayExpression.innerHTML += (e.target.textContent);
-            canUseDot = true;   
-            console.log("does not have dot")
+    else if (!operators.includes(e.target)) {
+        if (e.target !== dot) {
+            // if we are pressing a number and the display has an operator followed by zero at the end :-eg +0
+            //  then replace the zero with whatever number we are typing
+
+            if (operatorStrings.includes(secLastCharacterOnDisplay) && lastCharacterOnDisplay == "0") {
+                displayExpression.innerHTML = displayExpression.innerHTML.slice(0, -1) + e.target.innerHTML;
+            }
+            else {
+                displayExpression.innerHTML += (e.target.textContent);
+                canUseDot = true;
+            }
         }
-        else{
-               if (canUseDot == true){
+        else {
+            if (canUseDot == true) {
                 displayExpression.innerHTML += (e.target.textContent);
                 canUseDot = false;
-               }
-               else{
-                   rejectKey(e.target)
-               }
+            }
+            else rejectKey(e.target)
         }
     }
-    else{
+    else {
         canUseDot = false;
-        if ( displayExpression.innerHTML == ""){
-            rejectKey(e.target);
-        }
-        else
-            consecutiveOperators(e.target, displayExpression.innerHTML)
+        if (displayExpression.innerHTML == "") rejectKey(e.target);
+        else consecutiveOperators(e.target, displayExpression.innerHTML)
     }
 }));
 
-equalSign.addEventListener("click", e => {
 
-    const expression =  displayExpression.innerHTML;
-    soundEffect.play()
-    return evaluate(expression);
-});
 
 // add functions
 const consecutiveOperators = (pressedKey, expression) => {
-    console.log("functioon coperators ")
-    if (!(operatorStrings.includes(expression[expression.length - 1]))){
-        if ((expression[expression.length - 1]) == dot){
-            rejectKey(pressedKey);
-        }
-        else
-            displayExpression.innerHTML += pressedKey.innerHTML;
-     }
-    else if (!(operatorStrings.includes(expression[expression.length - 2])) ){
-        console.log("ony last one is operator")
-        if (pressedKey.innerHTML == "÷" || pressedKey.innerHTML == "*"){
-            rejectKey(pressedKey);
-        }
-        else
-            displayExpression.innerHTML += pressedKey.innerHTML
+    // if last character on display is not an operator but a dot reject key
+    //but if the last character is a number the allow the user to input the operator
+    console.log("1")
+    if (!(operatorStrings.includes(expression[expression.length - 1]))) {
+        if ((expression[expression.length - 1]) == dot) rejectKey(pressedKey);
+        else displayExpression.innerHTML += pressedKey.innerHTML;
     }
-    else if (operatorStrings.includes(expression[expression.length - 1]) && operatorStrings.includes(expression[expression.length - 2])){
 
-        if (pressedKey.innerHTML == "÷" || pressedKey.innerHTML == "*"){
-            rejectKey(pressedKey);
-        }
-        else
+    // if only the last character is an operator 
+    // then replace it with the key we are pressing
+    // but if it is a minus then allow the user to input it 
+    else if (!(operatorStrings.includes(expression[expression.length - 2]))) {
+        if ((operatorStrings.includes(expression[expression.length - 1])) && pressedKey.innerHTML !== "-")
             displayExpression.innerHTML = expression.slice(0, -1) + pressedKey.innerHTML;
+        else displayExpression.innerHTML += pressedKey.innerHTML
     }
-    else
-        console.log("done last else")
+    else rejectKey(pressedKey)
 }
 
-// const rejectKey = (key) =>{
-//     key.classList.add("reject");
+equalSign.addEventListener("click", e => {
+    const expression = displayExpression.innerHTML;
+    soundEffect.play()
+    evaluate(expression);
+    displayExpression.innerHTML = displayResults.innerHTML;
+});
 
-//     setinterval(key.classList.remove("reject"), 300)
-
-    
 clear.addEventListener("click", e => {
     displayExpression.innerHTML = "";
     displayResults.innerHTML = ""
@@ -142,58 +95,24 @@ del.addEventListener("click", e => {
     console.log("hello")
     displayExpression.innerHTML = displayExpression.innerHTML.slice(0, -1)
 })
+const rejectKey = (key) => {
     key.classList.remove("reject");
-    setTimeout(
-      function() {
-        key.classList.add("reject");
-      },
-      25
-    );
-  }
-
-// check for unnesessary zeros
-
-// PROBLEMS:
-// no zero should appear before a number
-// no zeros should appear before a dp without a closing digit at the end of them
-// not more than one zero should appear before a dot
-// no more than one zero should appear on its own
-
-// SOLUTIONS
-// if pressedkey = 0; and the character before it is an operator
-// then set our variable(canInputZero) to false so that we cannot add more zeros to it 
-// if the pressed is a non-zero number and xracter before it is a zero
-// the slice the expression to remove the zero and replace it with the pressed key
-
-// (displayExpression.innerHTML[displayExpression.innerHTML.length - 1] == "0") 
-//         && operatorStrings.includes(displayExpression.innerHTML[displayExpression.innerHTML.length - 1]))
-
-const canIPressZero = (expression) =>{
-
-    if (operatorStrings.includes(displayExpression.innerHTML[displayExpression.innerHTML.length-1])){
-        canInputZero = true;
-    }
-    else if ( expression.length == 1 && expression == "0"){
-        canInputZero = false;
-    }
-    else if ( operatorStrings.includes(expression[expression.length -2]) && expression[expression.length -1] == "0"
-    )
-    {
-        canInputZero = false;
-    }
-
+    setTimeout( () => key.classList.add("reject"),25);
 }
+
+const canIPressZero = (expression) => {
+    return (operatorStrings.includes(displayExpression.innerHTML[displayExpression.innerHTML.length - 1])) ? canInputZero = true
+        : (expression.length == 1 && expression == "0") ? canInputZero = false
+            : (operatorStrings.includes(expression[expression.length - 2]) && expression[expression.length - 1] == "0") ? canInputZero = false
+                : canInputZero = true;
+}
+
 const checkForZeros = (pressedKey, expression) => {
     canIPressZero(expression)
-    console.log("can i input zero")
-    if(canInputZero == true){
-        displayExpression.innerHTML += pressedKey.innerHTML
-    }
-    else{
-        rejectKey(pressedKey)
-        displayExpression.innerHTML = expression.slice(0, -1) + pressedKey.innerHTML;
 
-    }
+    return (canInputZero == true) ? displayExpression.innerHTML += pressedKey.innerHTML
+        : rejectKey(pressedKey),
+        displayExpression.innerHTML = expression.slice(0, -1) + pressedKey.innerHTML;
 }
 
 
@@ -220,7 +139,7 @@ const parseExpressionWithPlus = (expression) => {
     const plusSeparatedNums = expression.split("+")
     const numbers = plusSeparatedNums.map(nums => parseExpressionWithMultiplication(nums));
     const initialValue = 0.0;
-	const result = numbers.reduce((acc, nums) => acc + nums, initialValue);
+    const result = numbers.reduce((acc, nums) => acc + nums, initialValue);
     return result;
 }
 
